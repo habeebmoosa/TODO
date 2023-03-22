3f6b592bf6f956c48c0b609c7d296180e536b0d1
Habeeb Moosa <habeebmoosadev@gmail.com>
2023-03-22 16:43:47 +0530

    Fix some issues and make this as a single page application

--- app/src/main/java/com/example/todo/MainActivity.java
+++ app/src/main/java/com/example/todo/MainActivity.java
@@ -1,35 +1,77 @@
 package com.example.todo;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
+
+import android.content.DialogInterface;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     ImageButton imageButton;
     ListView listView;
+    ArrayList<String> arrayList;
+    ArrayAdapter arrayAdapter;
+    EditText inputText;
+    String s;
+    AlertDialog.Builder builder;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         imageButton = (ImageButton) findViewById(R.id.imageButton);
-        Intent intent = new Intent(this, MainActivity2.class);
-        listView = findViewById(R.id.listView);
-        ArrayAdapter obj = new ArrayAdapter(this, android.R.layout.simple_list_item_1);
-        MainActivity2 ma2 = new MainActivity2();
-//        obj.add(ma2.text);
-//        listView.setAdapter(obj);
+        listView = (ListView) findViewById(R.id.listView);
+        arrayList = new ArrayList<>();
+        arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line,arrayList);
+        listView.setAdapter(arrayAdapter);
+        inputText = findViewById(R.id.inputText);
+        builder = new AlertDialog.Builder(this);
+
+
         imageButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(intent);
-//                obj.add(ma2.text);
+                s = inputText.getText().toString();
+                arrayList.add(s);
 
+                InputMethodManager imm = (InputMethodManager)getSystemService(INPUT_METHOD_SERVICE);
+                imm.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+                inputText.setText("");
+            }
+        });
+
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> arg0, View arg1, int pos, long id) {
+                builder.setTitle("DELETE");
+                builder.setMessage("Do you want to delete this task?");
+                builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        arrayList.remove(pos);
+                    }
+                });
+                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.cancel();
+                    }
+                });
+                builder.show();
+
+                return true;
             }
         });
     }
